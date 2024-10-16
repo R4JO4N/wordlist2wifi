4f4a2bc689344456f00d38aba25d8ab216fecdb1
R4JO4N <45194923+R4JO4N@users.noreply.github.com>
2024-10-16 11:54:35 +0600

    Delete filterrock.py

--- filterrock.py
+++
@@ -1,9 +0,0 @@
-#author: rajoan
-
-#Please change 'rockyou.txt' to your file name if your file name is different.(for most cases it is)
-           
-with open('rockyou.txt', 'r', encoding='utf-8', errors='ignore') as infile, open('filtered.txt','w',encoding='utf-8', errors='ignore') as outfile:
-    for line in infile:
-        password=line.strip()
-        if len(password)>=8:
-            outfile.write(password+'\n')
